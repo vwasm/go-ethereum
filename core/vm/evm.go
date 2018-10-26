3f221d6882bc9e75251d450e3833d4e815611324
Paweł Bylica <chfast@gmail.com>
2018-10-26 13:50:54 +0200

    core/vm: Allow using EVMC also as an EVM

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -147,8 +147,12 @@ func NewEVM(ctx Context, statedb StateDB, chainConfig *params.ChainConfig, vmCon
 		}
 	}
 
-	// vmConfig.EVMInterpreter will be used by EVM-C, it won't be checked here
-	// as we always want to have the built-in EVM as the failover option.
+	if vmConfig.EVMInterpreter != "" {
+		// Create custom EVM.
+		evm.interpreters = append(evm.interpreters, NewEVMC(vmConfig.EVMInterpreter, evm))
+	}
+
+	// Keep the built-in EVM as the failover option.
 	evm.interpreters = append(evm.interpreters, NewEVMInterpreter(evm, vmConfig))
 
 	return evm
